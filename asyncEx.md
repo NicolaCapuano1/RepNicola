34613e38b0620f1bede64770c092157b47b9a1ab
CapuanoNicola1 <capuanico1313@gmail.com>
2025-04-09 13:07:19 +0200

    ex.promise_all

---
+++ asyncEx.md
@@ -0,0 +1,36 @@
+﻿# Javascript async advanced
+
+Hai a disposizione una funzione chiamata `getPastEvent`, che restituisce una _Promise_ con un evento storico casuale sotto forma di oggetto:
+```
+function getPastEvent() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const events = [
+        { year: 1990, name: "World Wide Web Invented" },
+        { year: 2001, name: "Wikipedia Launched" },
+        { year: 1969, name: "Moon Landing" },
+        { year: 2020, name: "Global Pandemic" },
+        { year: 1989, name: "Fall of Berlin Wall" }
+      ];
+      const randomIndex = Math.floor(Math.random() * events.length);
+      resolve(events[randomIndex]);
+    }, Math.random() * 1000);
+  });
+}
+
+```
+
+### Obiettivi:
+Scrivi una funzione asincrona `travelThroughHistory`, che:
+
+-   Chiama **N volte** `getPastEvent()` in parallelo. ( N puo essere 5,10, 100, 1000 )
+    
+-   Raccoglie tutti i risultati.
+    
+-   Ordina gli eventi **cronologicamente (dal più vecchio al più recente)**.
+    
+-   Rimuove i **duplicati** (stesso anno + stesso nome).
+    
+-   Restituisce **solo** gli eventi avvenuti **prima del 2000**.
+
+
---
+++ ex1.js
@@ -0,0 +1,49 @@
+function getPastEvent() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const events = [
+                { year: 1990, name: "World Wide Web Invented" },
+                { year: 2001, name: "Wikipedia Launched" },
+                { year: 1969, name: "Moon Landing" },
+                { year: 2020, name: "Global Pandemic" },
+                { year: 1989, name: "Fall of Berlin Wall" }
+            ];
+            const randomIndex = Math.floor(Math.random() * events.length);
+            resolve(events[randomIndex]);
+        }, Math.random() * 1000);
+    });
+}
+
+n = Math.floor(Math.random() * 100000);
+console.log(n);
+
+async function travelThroughHistory(){
+    let promises = [];
+    for(i=0; i<n; i++){
+        promises.push(getPastEvent());
+    } 
+    let arr = await Promise.all(promises);
+    console.log("                                                  ");
+    console.log("**************** TUTTI GLI EVENTI ****************");
+    console.log("**************************************************");
+    console.log("                                                  ");
+    console.log(arr);
+
+    arr.sort((a, b) => a.year - b.year);
+    console.log("                                                  ");
+    console.log("************ EVENTI ORDINATI PER ANNO ************");
+    console.log("**************************************************");
+    console.log("                                                  ");
+    console.log(arr);
+
+    let filtarr = arr.filter(c => c.year <= 2000);
+    
+    filtarr = Array.from(new Set(filtarr.map(JSON.stringify))).map(JSON.parse);
+    console.log("                                                  ");
+    console.log("************** EVENTI FINO AL 2000! **************");
+    console.log("**************************************************");
+    console.log("                                                  ");
+    console.log(filtarr);
+}
+
+travelThroughHistory();
